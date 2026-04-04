7563cf0d9a28cc7c7f6eb1573d58b85b79d0339f
Ashutosh Jha <ashujha707070@gmail.com>
2026-04-04 15:42:49 +0530

    feat: setup inngest development server and event bus

---
+++ app/api/inngest/route.ts
@@ -0,0 +1,16 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/inngest/client";
+import { helloWorld, generateVideo } from "@/inngest/functions";
+import { seriesScheduler, dailyWorkflow } from "@/inngest/scheduling";
+
+console.log(">>> Inngest route handler loaded. Using Client ID: vidmaxx");
+
+export const { GET, POST, PUT } = serve({
+    client: inngest,
+    functions: [
+        helloWorld,
+        generateVideo,
+        seriesScheduler,
+        dailyWorkflow,
+    ],
+});
